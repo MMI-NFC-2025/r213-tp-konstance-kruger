d1d4234642903be8806609bb8c5ab4c28cf2e175
konstance-kruger <konstance.kruger@edu.univ-fcomte.fr>
2026-03-06 11:51:52 +0100

    tp6

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -1,4 +1,7 @@
 import PocketBase from 'pocketbase';
+export async function setFavori(house) {
+    await db.collection('maison').update(house.id, {favori: !house.favori});
+}
 
 const db = new PocketBase('http://127.0.0.1:8090');
 
@@ -31,5 +34,39 @@ export async function addOffre(house) {
             success: false,
             message: 'Une erreur est survenue en ajoutant la maison'
         };
+    }
+}
+
+export async function getAgents() {
+    try {
+        let data = await db.collection('agent').getFullList({
+            sort: '-created',
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des agents', error);
+        return [];
+    }
+}
+
+export async function getAgent(id) {
+    try {
+        const data = await db.collection('agent').getOne(id);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant l\'agent', error);
+        return {};
+    }
+}
+
+export async function getOffresByAgent(agentId) {
+    try {
+        const data = await db.collection('Maison').getFullList({
+            filter: `agent = "${agentId}"`
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant les offres de l\'agent', error);
+        return [];
     }
 }
